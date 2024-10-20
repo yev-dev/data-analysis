96c1fd05ee49f7c7c01178c545cb967897c2fb98
Yevgeniy Yermoshin <yev.developer@gmail.com>
2024-10-20 12:02:04 +0100

    Cleanup

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,4 +1,20 @@
 
 
 def test_simple_stuff():
-    assert True
+    
+    L = [['settlement',20200303],['maturity',20200304]]
+
+    # for i, x in enumerate(L):
+    #     for j, a in enumerate(x):
+    #         if 'bob' in a:
+    #             L[i][j] = a.replace('bob', 'b')
+
+    print(L)
+
+    values = ['settlement',20240303]
+
+    settlement = 20240303
+
+    new_values = [ [i,settlement if i == 'settlement' else j] for i, j in L ]
+
+    assert new_values == [['settlement', 20240303], ['maturity', 20200303]]
